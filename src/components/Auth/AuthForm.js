8f409e7b61c54ab2d89f7b7a44be310599cbb46c
ShibinaNoushad <shibinanoushad2224@gmail.com>
2023-01-16 21:26:35 +0530

    Login functionality Added

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -22,36 +22,46 @@ const AuthForm = () => {
     // optional: Add validation
 
     setIsLoading(true);
+    let url;
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBykkHuQGFHYvxHkPjT9Uvhn4pMOHOxuE4";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBykkHuQGFHYvxHkPjT9Uvhn4pMOHOxuE4",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBykkHuQGFHYvxHkPjT9Uvhn4pMOHOxuE4";
+    }
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
-          // ...
+          return res.json();
         } else {
           return res.json().then((data) => {
             let errorMessage = "Authentication failed!";
             if (data && data.error && data.error.message) {
               errorMessage = data.error.message;
             }
-            alert(errorMessage);
+
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((dat) => {
+        console.log(dat);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
-    }
   };
 
   return (
